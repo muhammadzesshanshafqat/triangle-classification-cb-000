00cc1f4f8b2d92dd2e79b707837d1eb37cbbe534
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-10-11 09:30:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    #binding.pry
+    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side3 + @side1 <= @side2
+      raise TriangleError
+    elsif @side1 < 0 || @side2 < 0 || @side3 < 0
+      raise TriangleError
+    elsif @side1 == 0 && @side2 == 0 && @side3 == 0
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3 && @side1 == @side3
+      :equilateral
+    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      :scalene
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3 && @side1 != @side2 || @side2 != @side3 || @side1 != @side3
+      :isosceles
+
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "Triangle error. Please check input values for all sides of the triangle."
+  end
 end
